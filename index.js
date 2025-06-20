ef5f92e2ba26c564eda0e64014593d0c4926e314
Parv Tiwari <parvtiwari1@gmail.com>
2025-06-20 11:59:19 +0530

    Connect to Backend

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
 let express = require("express");
+let path = require("path");
 let app = express();
 
 let port = 8080;
+
+app.use(express.static(path.join(__dirname, "/public/CSS")));
+app.use(express.static(path.join(__dirname, "/public/JS")));
+app.use(express.static(path.join(__dirname, "/public/images")));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+
+app.get('/', (req, res)=>{
+    res.render("login.ejs");
+});
+app.get("/home", (req, res)=>{
+    res.render("index.ejs");
+});
+app.get("/menu", (req, res)=>{
+    res.render("menu.ejs");
+});
+app.get("/about", (req, res)=>{
+    res.render("about.ejs");
+});
+app.get("/cart", (req, res)=>{
+    res.render("cart.ejs");
+});
+app.get("/contact", (req, res)=>{
+    res.render("contact.ejs");
+});
+app.get("/payment", (req, res)=>{
+    res.render("payment.ejs");
+});
+// app.get("*", (req, res) => {
+//     res.render("index.ejs");
+// });
+
 app.listen(port, ()=>{
     console.log(`App is Listening on Port ${port}`);
 });

0cc153c0e4154735f67cd42f282fb5dec6aaf310
Parv Tiwari <139171300+ParvTiwari@users.noreply.github.com>
2025-07-25 18:00:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,12 +14,18 @@ app.use(session({
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: parseInt(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME
 });
-
+connection.connect((err) => {
+  if (err) {
+    console.error("Database connection failed:", err.stack);
+    return;
+  }
+  console.log("Connected to database.");
+});
 let port = 8080;
 
 app.use(express.static(path.join(__dirname, "public")));

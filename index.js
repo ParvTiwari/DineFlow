84353f34f4a2c1985dd3f90382cc874238a971ec
Parv Tiwari <139171300+ParvTiwari@users.noreply.github.com>
2025-07-25 17:56:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,10 +13,11 @@ app.use(session({
 }));
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'dineflow',
-  password: 'mysql@123'
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 });
 
 let port = 8080;
